consuldemocracy/consuldemocracy

Refactor segment constant into a class method
Remove warning due to reloading constant SEGMENTS[1]

Makes the definition of segments with geozones a little cleaner. I
think it’s worth it, compared to the slight memory gain of using a
constant[3]

[1] lib/user_segments.rb:2: warning: already initialized constant
UserSegments::SEGMENTS

[2]
https://stackoverflow.com/questions/15903835/class-method-vs-constant-in
-ruby-rails#comment55921926_15903970

[3]
https://stackoverflow.com/questions/15903835/class-method-vs-constant-in
-ruby-rails#answer-15903970

spec/lib/user_segments_spec.rb
@@ -228,6 +228,17 @@ describe UserSegments do
       expect(described_class).to_not respond_to("jupiter")
     end
 
+    it "includes geozones in available segments" do
+      expect(described_class.segments).to include("new_york")
+      expect(described_class.segments).to include("california")
+      expect(described_class.segments).to include("mars")
+      expect(described_class.segments).to_not include("jupiter")
+    end
+
+    it "does not include the generic city as a segment" do
+      expect(described_class.segments).to_not include("city")
+    end
+
     it "returns users of a geozone" do
       expect(described_class.new_york).to include(user1)
       expect(described_class.new_york).to include(user2)

zcw888/consul

spec/lib/user_segments_spec.rb
@@ -245,6 +245,13 @@ describe UserSegments do
       expect(described_class).not_to respond_to("jupiter")
     end
 
+    it "includes geozones in available segments" do
+      expect(described_class.segments).to include("new_york")
+      expect(described_class.segments).to include("california")
+      expect(described_class.segments).to include("mars")
+      expect(described_class.segments).not_to include("jupiter")
+    end
+
     it "returns users of a geozone" do
       expect(described_class.new_york).to include(user1)
       expect(described_class.new_york).to include(user2)