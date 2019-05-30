consuldemocracy/consuldemocracy

Adds geozones as user segments

spec/lib/user_segments_spec.rb
@@ -206,4 +206,42 @@ describe UserSegments do
     end
   end
 
+  context "Geozones" do
+
+    let!(:new_york) { create(:geozone, name: "New York") }
+    let!(:california) { create(:geozone, name: "California") }
+    let!(:mars) { create(:geozone, name: "Mars") }
+
+    let(:user1) { create(:user, geozone: new_york) }
+    let(:user2) { create(:user, geozone: new_york) }
+    let(:user3) { create(:user, geozone: california) }
+    let(:user4) { create(:user, geozone: mars) }
+
+    before do
+      load 'lib/user_segments.rb'
+    end
+
+    it "dynamically generates user segments for all geozones" do
+      expect(described_class).to respond_to("new_york")
+      expect(described_class).to respond_to("california")
+      expect(described_class).to respond_to("mars")
+      expect(described_class).to_not respond_to("jupiter")
+    end
+
+    it "returns users of a geozone" do
+      expect(described_class.new_york).to include(user1)
+      expect(described_class.new_york).to include(user2)
+      expect(described_class.new_york).to_not include(user3)
+      expect(described_class.new_york).to_not include(user4)
+    end
+
+    it "only returns active users of a geozone" do
+      user2.update(erased_at: Time.current)
+
+      expect(described_class.new_york).to include(user1)
+      expect(described_class.new_york).to_not include(user2)
+    end
+
+  end
+
 end

zcw888/consul

spec/lib/user_segments_spec.rb
@@ -223,4 +223,42 @@ describe UserSegments do
     end
   end
 
+  context "Geozones" do
+
+    let!(:new_york) { create(:geozone, name: "New York") }
+    let!(:california) { create(:geozone, name: "California") }
+    let!(:mars) { create(:geozone, name: "Mars") }
+
+    let(:user1) { create(:user, geozone: new_york) }
+    let(:user2) { create(:user, geozone: new_york) }
+    let(:user3) { create(:user, geozone: california) }
+    let(:user4) { create(:user, geozone: mars) }
+
+    before do
+      load "lib/user_segments.rb"
+    end
+
+    it "dynamically generates user segments for all geozones" do
+      expect(described_class).to respond_to("new_york")
+      expect(described_class).to respond_to("california")
+      expect(described_class).to respond_to("mars")
+      expect(described_class).not_to respond_to("jupiter")
+    end
+
+    it "returns users of a geozone" do
+      expect(described_class.new_york).to include(user1)
+      expect(described_class.new_york).to include(user2)
+      expect(described_class.new_york).not_to include(user3)
+      expect(described_class.new_york).not_to include(user4)
+    end
+
+    it "only returns active users of a geozone" do
+      user2.update(erased_at: Time.current)
+
+      expect(described_class.new_york).to include(user1)
+      expect(described_class.new_york).not_to include(user2)
+    end
+
+  end
+
 end