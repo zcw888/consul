50cf4fa0bc89e3f246fb25da97c7e325f3689512
decabeza <alberto@decabeza.es>
2020-04-21 14:56:03 +0200

    Filter erased users and show erase reason in admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,9 +1,13 @@
 class Admin::UsersController < Admin::BaseController
   load_and_authorize_resource
 
+  has_filters %w[active erased], only: :index
+
   def index
-    @users = User.by_username_email_or_document_number(params[:search]) if params[:search]
-    @users = @users.page(params[:page])
+    @users = User.send(@current_filter)
+                 .by_username_email_or_document_number(params[:search]) if params[:search]
+    @users = @users.send(@current_filter).page(params[:page])
+
     respond_to do |format|
       format.html
       format.js
