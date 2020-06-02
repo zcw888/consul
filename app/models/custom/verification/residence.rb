5144efe17298e69e2b682e47881e5b8756effd07
Javi Martín <javim@elretirao.net>
2020-06-02 23:35:52 +0200

    TODO: verification

--- app/models/custom/verification/residence.rb
+++ app/models/custom/verification/residence.rb
@@ -1,18 +1,23 @@
 require_dependency Rails.root.join("app", "models", "verification", "residence").to_s
 
 class Verification::Residence
-  validate :postal_code_in_madrid
-  validate :residence_in_madrid
+  validate :postal_code_tenant
+  validate :residence_tenant
+  attr_writer :tenant
 
-  def postal_code_in_madrid
+  def tenant
+    @tenant ||= Tenant.current
+  end
+
+  def postal_code_tenant
     errors.add(:postal_code, I18n.t("verification.residence.new.error_not_allowed_postal_code")) unless valid_postal_code?
   end
 
-  def residence_in_madrid
+  def residence_tenant
     return if errors.any?
 
     unless residency_valid?
-      errors.add(:residence_in_madrid, false)
+      errors.add(:residence_tenant, false)
       store_failed_attempt
       Lock.increase_tries(user)
     end
@@ -21,6 +26,6 @@ class Verification::Residence
   private
 
     def valid_postal_code?
-      postal_code =~ /^280/
+      postal_code =~ /^#{tenant.postal_code}/
     end
 end
