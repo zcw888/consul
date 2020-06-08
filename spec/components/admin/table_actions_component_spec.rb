d79c1896f7b9799a95897a5fcd1baadd94b3055e
Javi Martín <javim@elretirao.net>
2020-06-09 00:47:41 +0200

    Move table actions partial to a component
    This partial was going to get too complex since in some places we've got
    different texts, different URLs or different confirmation messages.
    While we should probably try to be more consistent and that would make
    the partial work in most cases, there'll always be some exceptions, and
    using a partial (with, perhaps, some helper methods) will become messy
    really quickly.

---
+++ spec/components/admin/table_actions_component_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+module Admin
+  describe TableActionsComponent, type: :component do
+    let(:banner) { create(:banner) }
+
+    it "renders a link to edit a record by default" do
+      render_inline TableActionsComponent.new(banner)
+
+      expect(page).to have_link "Edit"
+    end
+
+    it "renders a link to destroy a record by default" do
+      render_inline TableActionsComponent.new(banner)
+
+      expect(page).to have_link "Delete"
+    end
+
+    context "actions parameter is passed" do
+      it "renders a link to edit a record if passed" do
+        render_inline TableActionsComponent.new(banner, actions: [:edit])
+
+        expect(page).to have_link "Edit"
+        expect(page).not_to have_link "Delete"
+      end
+
+      it "renders a link to destroy a record if passed" do
+        render_inline TableActionsComponent.new(banner, actions: [:destroy])
+
+        expect(page).to have_link "Delete"
+        expect(page).not_to have_link "Edit"
+      end
+    end
+  end
+end
